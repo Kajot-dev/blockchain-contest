00213ed3348df1ba0b7f9ade76ae4fd214222bda
Albert <albertskonieczny@gmail.com>
2023-05-02 21:25:00 +0200

    chakra unwrap

--- blockchain-contest-front/src/pages/_app.js
+++ blockchain-contest-front/src/pages/_app.js
@@ -1,20 +1,11 @@
 import "@/styles/globals.css";
 import { MetaMaskProvider } from "metamask-react";
 import * as React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  initialColorMode: "dark",
-  useSystemColorMode: true,
-});
 
 export default function App({ Component, pageProps }) {
   return (
     <MetaMaskProvider>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <Component {...pageProps} />
     </MetaMaskProvider>
   );
 }
