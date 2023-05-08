c31e5eeb65d57d2e9d311b7f4bed2172f8553483
Kajot-dev <kuba10j@gmail.com>
2023-05-08 11:48:25 +0200

    ui/change logo and fix header

--- blockchain-contest-front/src/pages/index.js
+++ blockchain-contest-front/src/pages/index.js
@@ -1,15 +1,9 @@
-import Head from "next/head";
+import DefaultHead from "@/components/DefaultHead";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Couply</title>
-        <meta name="description" content="You best site for coupons!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <NavBar />
       <main></main>
     </>
