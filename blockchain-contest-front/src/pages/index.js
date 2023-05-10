4ed833b263c3d4ee7262ea6d9e685b753f96cf03
Albert <albertskonieczny@gmail.com>
2023-05-10 16:01:46 +0200

    fill-bg position fix and transparent navbar

--- blockchain-contest-front/src/pages/index.js
+++ blockchain-contest-front/src/pages/index.js
@@ -1,10 +1,20 @@
 import NavBar from "../components/NavBar";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <main></main>
+      <NavBar transparent={true} />
+      <main className={`${styles.home}`}>
+        <div className="fill-bg">
+          <div className="fill magic-bg" />
+        </div>
+        <section className={`${styles.heroSection}`}>
+          <div className="container">
+            <h1>Blockchain Contest</h1>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
