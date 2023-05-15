57886ef4d790f385955244cdbc4a8d9b7616c8a2
Kajot-dev <kuba10j@gmail.com>
2023-05-15 22:07:23 +0200

    chore/format

--- blockchain-contest-front/src/pages/zen.js
+++ blockchain-contest-front/src/pages/zen.js
@@ -7,13 +7,12 @@ const unbounded4 = Unbounded({ subsets: ["latin"], weight: "400" });
 const unbounded6 = Unbounded({ subsets: ["latin"], weight: "600" });
 
 export default function Home() {
-
   useEffect(() => {
-    console.log(document.documentElement)
-    document.documentElement.classList.add(styles.scrollType)
+    console.log(document.documentElement);
+    document.documentElement.classList.add(styles.scrollType);
     return () => {
-      document.documentElement.classList.remove(styles.scrollType)
-    }
+      document.documentElement.classList.remove(styles.scrollType);
+    };
   }, []);
 
   return (
@@ -25,28 +24,34 @@ export default function Home() {
         </div>
         <div className={`${styles.zenText} ${unbounded4.className}`}>
           <div className={styles.scrollStop}>
-            A <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>Platform with NFTs Launches</span>
+            A{" "}
+            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>
+              Platform with NFTs Launches
+            </span>
           </div>
-          <div>
-            {" for "}
+          <div>{" for "}</div>
+          <div className={styles.scrollStop}>
+            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>
+              Unique Physical Collectibles Enthusiasts, Traders and Investors
+            </span>
+          </div>
+          <div>{" that "}</div>
+          <div className={styles.scrollStop}>
+            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>
+              Undertakes Partnerships with Stores and Retailers assuring
+              Authenticity
+            </span>
+          </div>
+          <div>{" enabling, "}</div>
+          <div className={styles.scrollStop}>
+            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>
+              Pre-release trading, and providing broader B2B Services
+            </span>
           </div>
           <div className={styles.scrollStop}>
-            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>Unique Physical Collectibles Enthusiasts, Traders and Investors</span>
-          </div>
-          <div>
-            {" that "}
-          </div>
-          <div className={styles.scrollStop}>
-            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>Undertakes Partnerships with Stores and Retailers assuring Authenticity</span>
-          </div>
-          <div>
-            {" enabling, "}
-          </div>
-          <div className={styles.scrollStop}>
-            <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>Pre-release trading, and providing broader B2B Services</span>
-          </div>
-          <div className={styles.scrollStop}>
-            {" unlike "} <span className={`${unbounded6.className} ${styles.zenEmphasize}`}>RTFKT</span>
+            {" unlike "}<span className={`${unbounded6.className} ${styles.zenEmphasize}`}>
+              RTFKT
+            </span>
           </div>
         </div>
       </main>
