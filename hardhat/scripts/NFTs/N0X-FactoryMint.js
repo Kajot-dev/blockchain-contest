65ca5ffea0ea117ffeb33d8be197c6a6c7052f9a
Albert <albertskonieczny@gmail.com>
2023-05-18 21:38:26 +0200

    fix

--- hardhat/scripts/NFTs/N0X-FactoryMint.js
+++ hardhat/scripts/NFTs/N0X-FactoryMint.js
@@ -1,51 +1,42 @@
-const { ethers, getNamedAccounts } = require("hardhat")
-require('dotenv').config()
+const { ethers, getNamedAccounts } = require("hardhat");
+require("dotenv").config();
 
-let PROTOCOL_PREFIX = "ipfs://"
+let PROTOCOL_PREFIX = "ipfs://";
 
-let jsonNikeCIDs = [
-    process.env.NIKE_CID1,
-    process.env.NIKE_CID2,
-    process.env.NIKE_CID3,
-    process.env.NIKE_CID4,
-]
+let jsonNikeCIDs = [process.env.NIKE_CID1, process.env.NIKE_CID2, process.env.NIKE_CID3, process.env.NIKE_CID4];
 
 const tokenInfo = {
-    name: "Air",
-    symbol: "NC",
-}
+  name: "Air",
+  symbol: "NC",
+};
 
 async function main() {
+  console.log("Assigning user roles...");
 
-    console.log("Assigning user roles...")
+  const { nikeRetailer } = await getNamedAccounts();
+  const nikeDP = await ethers.getSigner(nikeRetailer);
 
-    const { nikeRetailer } = await getNamedAccounts()
-    const nikeDP = await ethers.getSigner(nikeRetailer)
+  const NFTFactory = await ethers.getContract("NFTFactory");
 
-    const NFTFactory = await ethers.getContract("NFTFactory")
+  console.log("Creating new NFT Contracts:");
+  await NFTFactory.connect(nikeDP).deployToken(tokenInfo.name, tokenInfo.symbol);
 
-    console.log("Creating new NFT Contracts:")
-    await NFTFactory.connect(nikeDP).deployToken(tokenInfo.name, tokenInfo.symbol)
+  console.log("Minting and uploading tokenURis!");
 
-    console.log("Minting and uploading tokenURis!")
+  for (let i = 0; i < jsonNikeCIDs.length; i++) {
+    iteratedFullURI = PROTOCOL_PREFIX + jsonCIDs[i];
 
-    for (let i = 0; i < jsonNikeCIDs.length(); i++) {
+    const NFTMinter = await NFTFactory.connect(nikeDP).mintNFT(0, iteratedFullURI);
+    await NFTMinter.wait(1);
 
-        iteratedFullURI = PROTOCOL_PREFIX + jsonCIDs[i]
-
-        const NFTMinter = await NFTFactory.connect(nikeDP).mintNFT(0, iteratedFullURI)
-        await NFTMinter.wait(1)
-
-        const tokenUriByIndex = await NFTFactory.showTokenUri(0, i)
-        console.log(`URI of token #${i} is ${tokenUriByIndex}`)
-
-    }
-
+    const tokenUriByIndex = await NFTFactory.showTokenUri(0, i);
+    console.log(`URI of token #${i} is ${tokenUriByIndex}`);
+  }
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
